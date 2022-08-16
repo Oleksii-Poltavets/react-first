9c0a1f7a4f04d90ca89bc90e944f5be98fc9a4ec
Oleksii Poltavets <rolinggman@gmail.com>
2022-08-17 00:04:19 +0300

    upload on github

---
+++ src/Technologies.js
@@ -0,0 +1,15 @@
+const Technologies = () => {
+    return (
+        <div>
+            <h1>What is my minimum stack?</h1>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>React</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Technologies;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <footer className='footer'>
+            <img className="logo" src="././images/logo.png" alt="logo" />
+        </footer>
+    )
+}
+
+export default Header;
